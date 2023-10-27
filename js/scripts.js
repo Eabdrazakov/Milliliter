0c08a08edd16c01d4689599a8464e9373a50c8de
Padma Patil <padma@email.com>
2023-10-27 10:44:48 -0700

    Done

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,13 @@ function teaToml(tsp) {
     return (tsp) * 4.928922;
 }
 
+function tableToml(tbsp) {
+    return (tbsp) * 14.786765;
+}
+
 //user interface logic
 const Ml = prompt("Enter tsp value");
+const tbsp = prompt("Enter tbsp value");
 
-window.alert('The result of tsp to (ml): ' + teaToml(Ml) + '.');
+window.alert('The result of tsp to (ml): ' + teaToml(Ml) + '.\n' +
+    'The result of tbsp to (ml): ' + tableToml(tbsp) + '.');
