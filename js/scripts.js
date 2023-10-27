7624854fcba44ff1a3dd1b105cd1f4e487dd0152
Padma Patil <padma@email.com>
2023-10-27 10:49:02 -0700

    Add toFixed

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,5 @@ function tableToml(tbsp) {
 const Ml = prompt("Enter tsp value");
 const tbsp = prompt("Enter tbsp value");
 
-window.alert('The result of tsp to (ml): ' + teaToml(Ml) + '.\n' +
-    'The result of tbsp to (ml): ' + tableToml(tbsp) + '.');
+window.alert('The result of tsp to (ml): ' + teaToml(Ml).toFixed(3) + '.\n' +
+    'The result of tbsp to (ml): ' + tableToml(tbsp).toFixed(4) + '.');
